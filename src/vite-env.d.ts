47c7ee3f7e3386cd7322cf0a8d4d030bae8b9bb9
danieldonoso1212 <70914446+danieldonoso1212@users.noreply.github.com>
2026-05-05 22:42:41 -0500

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
-/// <reference types="vite/client" />
-
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_ANON_KEY: string
+  readonly VITE_CALLMEBOT_PHONE: string
+  readonly VITE_CALLMEBOT_APIKEY: string
 }
 
 interface ImportMeta {
